cc2a5ff4a51d8d0cf6b3fb5104328df5781f2c22
山本大志 <yamamototaishi@yamamotohiroshinonotobukkukonpyuta.local>
2025-02-17 15:42:19 +0900

    setting

--- app/main.go
+++ app/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ping-pong-pong-ping",
+		})
+	})
+	r.Run()
 }
