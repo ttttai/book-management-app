1d8b9566bf679a247f9a05dbe3478fb60aaacbdb
山本大志 <yamamototaishi@yamamotohiroshinonotobukkukonpyuta.local>
2025-02-21 00:45:49 +0900

    timezone setting

--- app/infra/db.go
+++ app/infra/db.go
@@ -1,15 +1,21 @@
 package infra
 
 import (
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func NewDB() (*gorm.DB, error) {
-	dsn := "user:password@tcp(db:3306)/test_database?charset=utf8&parseTime=true&loc=Local"
+	dsn := "user:password@tcp(db:3306)/test_database?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
+
+	// タイムゾーンをJSTに設定
+	loc, _ := time.LoadLocation("Asia/Tokyo")
+	time.Local = loc
 	return db, err
 }
