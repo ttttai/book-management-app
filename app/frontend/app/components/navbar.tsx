6440cb27626930c324b5977d5504d8b5fe73ddeb
山本大志 <boysbeambitious_0328@outlook.jp>
2025-03-03 11:42:25 +0900

    refactoring

--- app/frontend/app/components/navbar.tsx
+++ app/frontend/app/components/navbar.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { usePathname } from "next/navigation";
 import { Bot, CircleUserRound, Library, Search } from "lucide-react";
 import Link from "next/link";
 
 export default function Navbar() {
-  const router = useRouter();
   const pathname = usePathname();
 
   // 現在のページのボタンに適用するクラス
