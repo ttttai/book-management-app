7efbd6d81f6e52f187b1d2fc502d055f649c8a90
山本大志 <boysbeambitious_0328@outlook.jp>
2025-03-04 18:43:37 +0900

    vercel.json

--- app/frontend/app/recommendation/page.tsx
+++ app/frontend/app/recommendation/page.tsx
@@ -1,6 +1,4 @@
-// import { useEffect, useState } from "react";
 import { Bot } from "lucide-react";
-// import Loading from "./loading";
 import BookInfoDisplay from "../components/bookInfoDisplay";
 import camelcaseKeys from "camelcase-keys";
 
