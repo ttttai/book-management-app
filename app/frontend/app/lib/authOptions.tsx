2fec6d8f9969a6f03ac60a5151c6b549d7f8b47c
山本大志 <boysbeambitious_0328@outlook.jp>
2025-04-11 22:38:16 +0900

    authentication fix

---
+++ app/frontend/app/lib/authOptions.tsx
@@ -0,0 +1,55 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { hash, compare } from "bcryptjs";
+
+const getAuthenticationByEmail = async (email: string) => {
+  const apiUrl = `${process.env.API_URL}/authentication/search?email=${email}`;
+  const res = await fetch(apiUrl);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch authentication info");
+  }
+  const data = await res.json();
+
+  return {
+    id: String(data.ID),
+    email: data.Email,
+    password: data.Password,
+  };
+};
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        console.log("aaaaa");
+        console.log("credentials!.email", credentials!.email);
+        const authentication = await getAuthenticationByEmail(
+          credentials!.email
+        );
+        if (!authentication) {
+          return null;
+        }
+
+        const isValid = await compare(
+          credentials!.password,
+          authentication.password
+        );
+        if (!isValid) {
+          return null;
+        }
+
+        return { id: authentication.id, email: authentication.email };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+};
--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import { Search, Library, Bot } from "lucide-react";
 import Link from "next/link";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/authOptions";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
